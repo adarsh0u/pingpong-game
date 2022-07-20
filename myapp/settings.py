e9b5cd445daf05f204fb2af7a116af2edb841d8f
adarsh0u <asmitakumari00019@gmail.com>
2022-07-20 16:05:52 +0530

    fix bugs

--- myapp/settings.py
+++ myapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q*lzgi6^6^#bd6_4^rap%7#9jcq)zdi3zep&irya@x9=kvbffp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
