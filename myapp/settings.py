694f50349bbf6821217531a48bacac5c5838b1f5
adarsh0u <asmitakumari00019@gmail.com>
2022-07-31 11:51:25 +0530

    fix bugs

--- myapp/settings.py
+++ myapp/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,4 +131,3 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #for bootstrap connection
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-django_heroku.settings(locals())
